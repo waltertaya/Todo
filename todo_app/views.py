eee911871976efb14798446c27a610949618b1f7
lewmas9152 <smnjuguna22@gmail.com>
2024-06-03 10:15:07 +0300

    Done with the templates

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo_app index.")
+    return render(request, 'todo_app/landing.html')
+
+def register(request):
+    return render(request, 'todo_app/register.html')
+
+def login(request):
+    return render(request, 'todo_app/login.html')   
+
+def home(request):
+    return render(request, 'todo_app/home.html')
 
 # Create your views here.
