7bb83302d9169f2dbf15a5299d41e6405e1b4f87
lewmas9152 <smnjuguna22@gmail.com>
2024-06-07 12:36:55 +0300

    corrected db name

--- todoprj/settings.py
+++ todoprj/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'todoprj.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://postgres:postgres@localhost:5432/tododb',
+    'default': dj_database_url.config(default='postgres://postgres:postgres@localhost:5432/tododj',
      conn_max_age=600)
 }
 
